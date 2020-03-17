d962c3bc2aa1a864fb9063a8ee317f3601f21f21
yamacho <yamacho@users.noreply.github.com>
2020-03-17 17:37:06 +0900

    Bootstrap を利用するように変更

--- app/entry.js
+++ app/entry.js
@@ -4,6 +4,8 @@ import $ from 'jquery';
 const global = Function('return this;')();
 global.jQuery = $;
 
+import bootstrap from 'bootstrap';
+
 $('.availability-toggle-button').each((i, e) => {
   const button = $(e);
   button.click(() => {
